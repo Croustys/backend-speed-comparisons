e499105ad9c546badaa2f0c92d560f8cc42b09e3
Croustys <akos.barabas2002@gmail.com>
2022-03-31 15:39:41 +0200

    init + node

---
+++ node/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const PORT = process.env.PORT || 4567;
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Server Working as intended"));
+app.get("/ping", (req, res) => res.json({ message: "PONG" }));
+app.get("/test", (req, res) => {
+  const startTime = new Date();
+  const total = 1000000000;
+  let testSum = 0;
+  for (let index = 0; index < total; index++) {
+    testSum++;
+  }
+  if (testSum === total) {
+    const endTime = new Date();
+    const runTime = endTime - startTime;
+    res.json({ complete: true, runTime: `${runTime} ms` });
+  } else res.status(500).json({ error: "error" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
